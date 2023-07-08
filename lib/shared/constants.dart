7c9d86e50fc87cb52c3fe855d0f39898f2c26a54
SreehariC <95119050+SreehariC@users.noreply.github.com>
2023-07-08 08:48:56 +0530

    first commit

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static String appId = "1:634480644405:web:5549258ea987afaefb7c7c";
-  static String apiKey = "AIzaSyADZ6Uq-pFQIWMdkGeh7JIozEY7jJM6r_I";
-  static String messagingSenderId = "634480644405";
-  static String projectId = "chatappflutter-13318";
+  static String apiKey = "AIzaSyCbOhet6hYk8aIWnIfy7qU2baC4MRBBEYg";
+  static String messagingSenderId = "644748761033";
+  static String projectId = "chatapp-fa92c";
   final primaryColor = const Color(0xFFee7b64);
 }
